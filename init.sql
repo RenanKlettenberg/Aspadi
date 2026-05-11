2e307b5a20cf80ec04a3a5e123490b9cd650394e
Renan G. Klettenberg <121028916+RenanKlettenberg@users.noreply.github.com>
2026-05-11 11:47:58 -0300

    fix: arrumando conexão com pgAdmin

--- init.sql
+++ init.sql
@@ -13,3 +13,16 @@ INSERT INTO animais.gato (nome, idade, cor) VALUES
 ('Luna', 3, 'cinza'),
 ('Garfield', 7, 'laranja'),
 ('Frajola', 4, 'preto e branco');
+
+CREATE SCHEMA IF NOT EXISTS "system";
+
+-- DROP TABLE system.sys_usuario 
+CREATE TABLE IF NOT EXISTS system.sys_usuario (
+	usu_id SERIAL NOT NULL,
+	usu_email VARCHAR(255) NOT NULL,
+	usu_nome VARCHAR(255) NOT NULL,
+	usu_password TEXT NOT NULL,
+	CONSTRAINT pk_sys_usuario_usu_id PRIMARY KEY (usu_id)
+);
+
+INSERT INTO system.sys_usuario (usu_email, usu_nome, usu_password) VALUES ('mestre@gmail.com', 'Mestre', '123');
